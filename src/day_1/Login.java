2d0f6473dd8085bc2b00f95b42fa66cb8356f0e3
hacers <hacer.karademir@gmail.com>
2020-05-12 17:24:08 +0100

    add Login.java

---
+++ src/day_1/Login.java
@@ -0,0 +1,14 @@
+package day_1;
+
+import java.util.Scanner;
+
+public class Login {
+    public static void main(String[] args) {
+        String name,password;
+        Scanner scn = new Scanner(System.in);
+        System.out.println(" Enter the username: ");
+        name = scn.next();
+        System.out.println(" Enter the password: ");
+        password = scn.next();
+    }
+}
