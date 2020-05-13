806cf812e588aa941c730994a48498d5396967a3
Hacer <hacers@users.noreply.github.com>
2020-05-13 15:57:43 +0100

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -3,6 +3,14 @@ package day_1;
 public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("new project");
+        
+        
+  String s= "a";
+if(s.contains("a")){
+    System.out.println("contains");
+}else{
+    System.out.println("not");
+}
 
     }
 }
